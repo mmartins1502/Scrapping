a3067973201da92690d3e62f9c6f9e38a5bf5958
Maxime Martins <maximemartins@macbook-pro-de-maxime.home>
2018-04-19 10:57:42 +0200

    final version

---
+++ GUI/src/Clear.java
@@ -0,0 +1,203 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Clear {
+
+	public Clear(String page, BufferedWriter result, String[] code) {
+		String raison = "Raison sociale";
+		String sexe = "Sexe";
+		System.out.printf("/////////////////////CLEAR HTML/////////////////////\n\n");
+		
+  		//////////////////////////////////////////////COPROPRIETE//////////////////////////////////////////////
+
+		if (page.contains(raison) == true)
+		{
+			System.out.printf("Raison Sociale\n\n");
+			
+			int Error = 0;
+			List<String> lines = new ArrayList<String>();
+      		lines.addAll(Arrays.asList(page.split("\\r?\\n")));
+      		
+      		
+      		
+
+      		//////////////////////////////////////////////SUPPRESSION DU HTML DE DEBUT DE PAGE//////////////////////////////////////////////
+  
+      		
+      		
+      		for (int i = 0; i < 40; i++) {
+      			if (lines.size() == 0) {
+      				System.out.printf("Liste vide ! %n");
+      				Error += 1;
+      				break;
+      			}
+      			else
+      				lines.remove(0);
+      		}
+      			
+          	
+      		//////////////////////////////////////////////SUPPRESSION DU HTML DE FIN DE PAGE//////////////////////////////////////////////
+      		
+      		int decount = lines.size();
+      		for (int i = decount - 1; i > (decount - 22); i--) {
+      			if (lines.size() == 0)
+      				break;
+      			else {
+      				lines.remove(i);
+      			}
+  			}
+      		
+      		
+      		/////////////////////////////////////////////SUPPRESSION DU HTML PARMIS LES INFOS///////////////////////////////////////////////
+      		
+
+      		
+      		StringBuilder sb = new StringBuilder();
+      		for (String s : lines)
+      		{
+      		    sb.append(s);
+      		    sb.append("\n");
+      		}
+      		String str = sb.toString();
+      		str = str.replace("				    <td width=\"35%\"><font face=\"Times New Roman\" size=\"2\">", "");
+  		    str = str.replace("					<td width=\"15%\"><font face=\"Times New Roman\" size=\"2\">", "");
+  		    str = str.replace("					<TD width=\"10%\"><FONT color=\"White\">", "");
+  		    str = str.replace("					<td width=\"5%\"><font face=\"Times New Roman\" size=\"2\">", "");
+  		    str = str.replace("					<td width=\"35%\" align=\"left\"><font face=\"Times New Roman\" size=\"2\">\n" + 
+  		    		"					", "");
+  		    str = str.replace("</FONT></td>", "");
+  		    str = str.replace("</FONT></TD>", "");
+  		    str = str.replace("</FONT>", "");
+  		    str = str.replace(" <br/>", "");
+  		    
+  		    str = str.replace("\n", ", ");
+  		    str = str.replace(", , ", ", ");
+  		    str = str.replace("  ", ", ");
+  		    str = str.replace(", , , , ", "\n");
+  		    str += "\n";
+  		    
+   
+  		    
+      		/////////////////////////////////////////////ECRITURE DES INFOS DANS UN FICHIER///////////////////////////////////////////////
+
+    
+  		    
+  		    System.out.println(str);
+  		    try {
+  					result.write("Parcelle : "+code[0]+" "+code[1]+" "+code[2]+" "+code[3]+"\n");
+  					result.write("Raison Sociale, Numero SIREN, Sigle, Droit, Adresse des titulaires de droit\n");
+  		    		if (Error == 0) {
+  		    			result.write(str);
+  		    		} else {
+  		    			result.write("Pas de Titulaires !\n");
+  		    		}
+  		    }
+  		    catch (IOException e)
+  		    {
+  			   e.printStackTrace();
+  		    }
+      	
+		}
+		
+  		//////////////////////////////////////////////MONOPROPRIETE//////////////////////////////////////////////
+
+		else if (page.contains(sexe) == true)
+		{
+			System.out.printf("Monopropriete\n\n");
+			
+			int Error = 0;
+			List<String> lines = new ArrayList<String>();
+      		lines.addAll(Arrays.asList(page.split("\\r?\\n")));
+      		
+      		
+      		
+
+      		//////////////////////////////////////////////SUPPRESSION DU HTML DE DEBUT DE PAGE//////////////////////////////////////////////
+  
+      		
+      		
+      		for (int i = 0; i < 50; i++) {
+      			if (lines.size() == 0) {
+      				System.out.printf("Liste vide ! %n");
+      				Error += 1;
+      				break;
+      			}
+      			else
+      				lines.remove(0);
+      		}
+      		
+      		
+      	
+      		//////////////////////////////////////////////SUPPRESSION DU HTML DE FIN DE PAGE//////////////////////////////////////////////
+      		
+      		int decount = lines.size();
+      		for (int i = decount - 1; i > (decount - 24); i--) {
+      			if (lines.size() == 0)
+      				break;
+      			else {
+      				lines.remove(i);
+      			}
+  			}
+
+   		
+      		
+      		
+      		/////////////////////////////////////////////SUPPRESSION DU HTML PARMIS LES INFOS///////////////////////////////////////////////
+      		
+
+      		
+      		StringBuilder sb = new StringBuilder();
+      		for (String s : lines)
+      		{
+      		    sb.append(s);
+      		    sb.append("\n");
+      		}
+      		String str = sb.toString();
+      		str = str.replace("				  <TD width=\"20%\"><FONT color=White>", "");
+  		    str = str.replace("\t\t\t\t<td width=\"20%\"><font face=\"Times New Roman\" size=\"2\">", "");
+  		    str = str.replace("\t\t\t\t<td width=\"3%\"><font face=\"Times New Roman\" size=\"2\">", "");
+  		    str = str.replace("\t\t\t\t<td width=\"10%\"><font face=\"Times New Roman\" size=\"2\">", "");
+  		    str = str.replace("\t\t\t\t<td width=\"15%\"><font face=\"Times New Roman\" size=\"2\">", "");
+  		    str = str.replace("\t\t\t\t<td width=\"29%\"><font face=\"Times New Roman\" size=\"2\">", "");
+  		    str = str.replace("\t\t\t\t<td width=\"29%\" align=\"left\"><font face=\"Times New Roman\" size=\"2\">", "");
+  		    str = str.replace("</FONT></td>", "");
+  		    str = str.replace("</FONT></TD>", "");
+  		    str = str.replace("</FONT>", "");
+  		    str = str.replace("					</td>", "");
+  		    str = str.replace("				</tr>", "");
+  		    str = str.replace("			 <tr align=\"middle\">", "\n");
+  		    str = str.replace("<br/>", "");
+  		    str = str.replace("\t", "");
+  		    str = str.replace("\n", ", ");
+  		    str = str.replace(", , ", ", ");
+  		    str = str.replace("  ", ", ");
+  		    str = str.replace(", , , , ", "\n");
+  		    str += "\n";
+  		    
+  		    
+      		/////////////////////////////////////////////ECRITURE DES INFOS DANS UN FICHIER///////////////////////////////////////////////
+
+  		    
+  		    
+  		    
+  		    System.out.println(str);
+  		    try {
+  					result.write("Parcelle : "+code[0]+" "+code[1]+" "+code[2]+" "+code[3]+"\n");
+  					result.write("Nom / Prénom, Sexe, Date de naissance, Lieu de naissance, Nom et prénom du conjoint, Droit, Adresse des titulaires de droit\n");
+  		    		if (Error == 0) {
+  		    			result.write(str);
+  		    		} else {
+  		    			result.write("Pas de Titulaires !\n");
+  		    		}
+  		    }
+  		    catch (IOException e)
+  		    {
+  			   e.printStackTrace();
+  		    }
+		}
+	}
+
+}
--- GUI/src/Fenetre.java
+++ GUI/src/Fenetre.java
@@ -27,6 +27,7 @@ public class Fenetre extends JFrame implements ActionListener{
 	private JOptionPane jop = new JOptionPane();
 	private JOptionPane jop2 = new JOptionPane();
 	private JOptionPane jop3 = new JOptionPane();
+	private JOptionPane jop4 = new JOptionPane();
 	private JFileChooser fc = new JFileChooser();
 	
 
@@ -48,106 +49,168 @@ public class Fenetre extends JFrame implements ActionListener{
 	    }
 		  //Méthode qui sera appelée lors d'un clic sur le bouton
 		public void actionPerformed(ActionEvent arg0) {
-	        String spdc = jop.showInputDialog(null, "Copiez votre cookie spdc ici !", "SPDC", JOptionPane.QUESTION_MESSAGE);
-	        String lemondgfip = jop2.showInputDialog(null, "Copiez votre cookie lemondgfip ici !", "lEMONDGFIP", JOptionPane.QUESTION_MESSAGE);
-	        String nb = jop3.showInputDialog(null, "Combien de secondes entre chaque requetes ?", "SECONDES", JOptionPane.QUESTION_MESSAGE);
+			String[] id = {"A00QWCCXVJ", "OARHV6SOGW"};
+			int type = JOptionPane.QUESTION_MESSAGE;
+			String utilisateur = (String)JOptionPane.showInputDialog(null, "Veuillez indiquer votre identifiant !", "Utilisateur", type, null, id, id[1]);
+			String spdc = JOptionPane.showInputDialog(null, "Copiez votre cookie spdc ici !", "SPDC", JOptionPane.QUESTION_MESSAGE);
+	        String lemondgfip = JOptionPane.showInputDialog(null, "Copiez votre cookie lemondgfip ici !", "lEMONDGFIP", JOptionPane.QUESTION_MESSAGE);
+	        String nb = JOptionPane.showInputDialog(null, "Combien de secondes entre chaque requetes ?", "SECONDES", JOptionPane.QUESTION_MESSAGE);
             int time = Integer.parseInt(nb);
 	        fc.showOpenDialog(null);
 	        File file = fc.getSelectedFile();	        
 	        try
 	        {
+//	        		System.out.printf("TRY \n");
+	        	
 	        		BufferedReader reader = new BufferedReader(new FileReader(file));
+	        		
+//	        		System.out.printf("Buffer OK\n");
+	        		
 	        		String line;
 	        	 	ArrayList<String> cookies = new ArrayList<String>();
-	        	 	cookies.add("jamyMailAdr=sebastien%2Edurand%40notaires%2Efr");
-	        	 	cookies.add("jamyDNStructure=uid%3DOKZCD1NP5U%2Cou%3DPRO65650%2Cou%3Dabonnes%2Ddgi%2Cou%3Dmefi%2Co%3Dgouv%2Cc%3Dfr");
-	        	 	cookies.add("perstrace=S+DURAND");
-	        	 	cookies.add("jamyDroits=T");
-	        	 	cookies.add("Annuaire=O");
-	        	 	cookies.add("jamyDNUser=uid%3DA00QWCCXVJ%2Cuid%3DOKZCD1NP5U%2Cou%3DPRO65650%2Cou%3Dabonnes%2Ddgi%2Cou%3Dmefi%2Co%3Dgouv%2Cc%3Dfr");
-	        	 	cookies.add("ckProfilcat=Consult");
-	        	 	cookies.add("ASPSESSIONIDCQRRRCCD=PMCHPFAABFIPLEDKNCBODCKN");
-	        	 	cookies.add("ASPSESSIONIDSSTRRDCD=ADJCBIAABMOGHDIGCLLKFOIO");
-	        	 	cookies.add("ASPSESSIONIDAQSTQCCC=ACIHMOHACFDKNIPLEFHFKPDO");
-	        	 	cookies.add("ASPSESSIONIDQQSQRDDD=BPEGNGJAIOJCFENJFMBIAHHL");
-	        	 	cookies.add("ASPSESSIONIDQQRSRDCB=CHGBEJHAMKOAMPOIKGHMOKKC");
-	        	 	cookies.add("CcodepM1=000");
-	        	 	cookies.add("CcocomM1=+++");
-	        	 	cookies.add("M1EnCours=0");
-	        	 	cookies.add("ASPSESSIONIDSSTRQDDD=AKMBLHLALOKJCCLHCKPHMDGD");
-	        	 	cookies.add("TypeStruct=O");
-	        	 	cookies.add("spdc="+spdc);
-	        	 	cookies.add("Office=OKZCD1NP5U");
-	        	 	cookies.add("Utilisateur=A00QWCCXVJ");
-	        	 	cookies.add("lemondgfip="+lemondgfip);
+
+//        	 		System.out.printf("Utilisateur unknown\n");
+//        	 		System.out.printf("Utilisateur = "+utilisateur+"\n");
+	        	 	
+        	 		String office ="";
+	        	 	if (utilisateur != null && utilisateur == "A00QWCCXVJ") {
+	        	 		
+//	        	 		System.out.printf("Config Utilisateur 1\n");
+	        	 		
+		        	 	cookies.add("jamyMailAdr=sebastien%2Edurand%40notaires%2Efr");
+		        	 	cookies.add("jamyDNStructure=uid%3DOKZCD1NP5U%2Cou%3DPRO65650%2Cou%3Dabonnes%2Ddgi%2Cou%3Dmefi%2Co%3Dgouv%2Cc%3Dfr");
+		        	 	cookies.add("perstrace=S+DURAND");
+		        	 	cookies.add("jamyDroits=T");
+		        	 	cookies.add("Annuaire=O");
+		        	 	cookies.add("jamyDNUser=uid%3DA00QWCCXVJ%2Cuid%3DOKZCD1NP5U%2Cou%3DPRO65650%2Cou%3Dabonnes%2Ddgi%2Cou%3Dmefi%2Co%3Dgouv%2Cc%3Dfr");
+		        	 	cookies.add("ckProfilcat=Consult");
+		        	 	cookies.add("ASPSESSIONIDCQRRRCCD=PMCHPFAABFIPLEDKNCBODCKN");
+		        	 	cookies.add("ASPSESSIONIDSSTRRDCD=ADJCBIAABMOGHDIGCLLKFOIO");
+		        	 	cookies.add("ASPSESSIONIDAQSTQCCC=ACIHMOHACFDKNIPLEFHFKPDO");
+		        	 	cookies.add("ASPSESSIONIDQQSQRDDD=BPEGNGJAIOJCFENJFMBIAHHL");
+		        	 	cookies.add("ASPSESSIONIDQQRSRDCB=CHGBEJHAMKOAMPOIKGHMOKKC");
+		        	 	cookies.add("CcodepM1=000");
+		        	 	cookies.add("CcocomM1=+++");
+		        	 	cookies.add("M1EnCours=0");
+		        	 	cookies.add("ASPSESSIONIDSSTRQDDD=AKMBLHLALOKJCCLHCKPHMDGD");
+		        	 	cookies.add("TypeStruct=O");
+		        	 	cookies.add("spdc="+spdc);
+		        	 	cookies.add("Office=OKZCD1NP5U");
+		        	 	cookies.add("Utilisateur="+utilisateur);
+		        	 	cookies.add("lemondgfip="+lemondgfip);
+		        	 	office = "OKZCD1NP5U";
+	        	 	}
+	        	 	else if (utilisateur == "OARHV6SOGW") {
+	        	 		
+//	        	 		System.out.printf("Config Utilisateur 2\n");
+	        	 		
+		        	 	cookies.add("jamyMailAdr=");
+		        	 	cookies.add("jamyDNStructure=uid%3D33O8K76WJG%2Cou%3DPRO65650%2Cou%3Dabonnes%2Ddgi%2Cou%3Dmefi%2Co%3Dgouv%2Cc%3Dfr");
+		        	 	cookies.add("perstrace=MAJAC+Pierre");
+		        	 	cookies.add("jamyDroits=T");
+		        	 	cookies.add("Annuaire=O");
+		        	 	cookies.add("jamyDNUser=uid%3DOARHV6SOGW%2Cuid%3D33O8K76WJG%2Cou%3DPRO65650%2Cou%3Dabonnes%2Ddgi%2Cou%3Dmefi%2Co%3Dgouv%2Cc%3Dfr");
+		        	 	cookies.add("ckProfilcat=Consult");
+		        	 	cookies.add("ASPSESSIONIDCQSTRBCB=JLALOPDCNEOKELGEDCJLEIMP");
+		        	 	cookies.add("ASPSESSIONIDAQRRQCAC=MEDAKFECMHJDFCPBIILIBLJH");
+		        	 	cookies.add("ASPSESSIONIDQCSRRDAC=GCDCIFECDJOJFHBJHOJALPOI");
+		        	 	cookies.add("ASPSESSIONIDSCTSTBAD=FBAIJFFCMDLIPMIIPOPGCJKM");
+		        	 	cookies.add("ASPSESSIONIDCQQRQADA=KOJBNKFCNBNAOABILFPFOMHF");
+		        	 	cookies.add("CcodepM1=000");
+		        	 	cookies.add("CcocomM1=+++");
+		        	 	cookies.add("M1EnCours=0");
+		        	 	cookies.add("TypeStruct=O");
+		        	 	cookies.add("spdc="+spdc);
+		        	 	cookies.add("Office=33O8K76WJG");
+		        	 	cookies.add("Utilisateur="+utilisateur);
+		        	 	cookies.add("lemondgfip="+lemondgfip);
+		        	 	office = "33O8K76WJG";
+		        	 	
+//	        	 		System.out.printf("Setting cookies : OK\n");
+	        	 	}
 	        	 	BufferedWriter result = new BufferedWriter(new FileWriter(new File("resultats.csv")));
+        	 		
+//	        	 	System.out.printf("Creating resultats.csv : OK\n");
+        	 		
 	        		while ((line = reader.readLine()) != null)
 	        		{
 	        			int Error = 0;
-	        			String[] code = line.split(";");
-	        			Request re = new Request();
-	    	          	String url = "http://spdc.dgfip.finances.gouv.fr/cdc_titulaires.asp?txtOffice=OKZCD1NP5U&txtCcodep=0"+code[0]+"&txtCcocom="+code[1]+"&txtSession="+spdc+"&txtUtilisateur=A00QWCCXVJ&txtCcoprf=&txtCcosec="+code[2]+"&txtDnupla=00"+code[3]+"&txtTypeTrait=10";
-		      		String page = re.GetPageContent(url, cookies, spdc, lemondgfip);
+	        			String[] code = line.split(", ");
+	        			
+//	        	 		System.out.printf("office ="+office+"\n");
+//	        	 		System.out.printf("code[0] ="+code[0]+"\n");
+//	        	 		System.out.printf("code[1] ="+code[1]+"\n");
+//	        	 		System.out.printf("code[2] ="+code[2]+"\n");
+//	        	 		System.out.printf("code[3] ="+code[3]+"\n");
+	        	 		
+        			Request re = new Request();
+    	          	String url = "http://spdc.dgfip.finances.gouv.fr/cdc_titulaires.asp?txtOffice="+office+"&txtCcodep=0"+code[0]+"&txtCcocom="+code[1]+"&txtSession="+spdc+"&txtUtilisateur="+utilisateur+"&txtCcoprf=&txtCcosec="+code[2]+"&txtDnupla=00"+code[3]+"&txtTypeTrait=10";
+        	 		
+//    	          	System.out.printf("Request : OK\n");
+        	 		
+    	          	String page = re.GetPageContent(url, cookies, spdc, lemondgfip);
+    	          	System.out.printf("/////////////////////////");
       				System.out.println(page);
-		      		List<String> lines = new ArrayList<String>();
-		      		lines.addAll(Arrays.asList(page.split("\\r?\\n")));
-		      		for (int i = 0; i < 50; i++) {
-		      			if (lines.size() == 0) {
-		      				System.out.printf("Liste vide ! %n");
-		      				Error += 1;
-		      				break;
-		      			}
-		      			else
-		      				lines.remove(0);
-		      		}
-		      		for (int i = 31; i > 8; i--) {
-		      			if (lines.size() == 0) {
-//		      				System.out.printf("Liste vide ! %n");
-		      				break;
-		      			}
-		      			else
-		      				lines.remove(i);		      		}
-
-		      		StringBuilder sb = new StringBuilder();
-		      		for (String s : lines)
-		      		{
-		      		    sb.append(s);
-		      		    sb.append("\n");
-		      		}
-		      		String str = sb.toString();
-	      		    str = str.replace("\t\t\t\t<td width=\"20%\"><font face=\"Times New Roman\" size=\"2\">", "");
-	      		    str = str.replace("\t\t\t\t<td width=\"3%\"><font face=\"Times New Roman\" size=\"2\">", "");
-	      		    str = str.replace("\t\t\t\t<td width=\"10%\"><font face=\"Times New Roman\" size=\"2\">", "");
-	      		    str = str.replace("\t\t\t\t<td width=\"15%\"><font face=\"Times New Roman\" size=\"2\">", "");
-	      		    str = str.replace("\t\t\t\t<td width=\"29%\"><font face=\"Times New Roman\" size=\"2\">", "");
-	      		    str = str.replace("\t\t\t\t<td width=\"29%\" align=\"left\"><font face=\"Times New Roman\" size=\"2\">", "");
-	      		    str = str.replace("</FONT></td>", "");
-	      		    str = str.replace("</FONT>", "");
-	      		    str = str.replace("<br/>", "");
-	      		    str = str.replace("\t", "");
-	      		    str = str.replace("\n", ", ");
-	      		    str = str.replace(", , ", ", ");
-	      		    str = str.replace("  ", ", ");
-	      		    str += "\n";
-	      		    System.out.println(str);
-	      		    try {
-	      		    		if (Error == 0) {
-	      		    			result.write(str);
-	      		    		} else {
-	      		    			result.write("Pas de Titulaires !\n");
-	      		    		}
-	      		    }
-	      		    catch (IOException e)
-	      		    {
-	      			   e.printStackTrace();
-	      		    }
+    	          	System.out.printf("/////////////////////////\n");
+      				Clear render = new Clear(page, result, code);
+//		      		List<String> lines = new ArrayList<String>();
+////		      		lines.addAll(Arrays.asList(page.split("\\r?\\n")));
+////		      		for (int i = 0; i < 50; i++) {
+////		      			if (lines.size() == 0) {
+////		      				System.out.printf("Liste vide ! %n");
+////		      				Error += 1;
+////		      				break;
+////		      			}
+////		      			else
+////		      				lines.remove(0);
+////		      		}
+////		      		for (int i = 31; i > 8; i--) {
+////		      			if (lines.size() == 0) {
+//////		      				System.out.printf("Liste vide ! %n");
+////		      				break;
+////		      			}
+////		      			else
+////		      				lines.remove(i);		      		}
+//
+//		      		StringBuilder sb = new StringBuilder();
+//		      		for (String s : lines)
+//		      		{
+//		      		    sb.append(s);
+//		      		    sb.append("\n");
+//		      		}
+//		      		String str = sb.toString();
+//	      		    str = str.replace("\t\t\t\t<td width=\"20%\"><font face=\"Times New Roman\" size=\"2\">", "");
+//	      		    str = str.replace("\t\t\t\t<td width=\"3%\"><font face=\"Times New Roman\" size=\"2\">", "");
+//	      		    str = str.replace("\t\t\t\t<td width=\"10%\"><font face=\"Times New Roman\" size=\"2\">", "");
+//	      		    str = str.replace("\t\t\t\t<td width=\"15%\"><font face=\"Times New Roman\" size=\"2\">", "");
+//	      		    str = str.replace("\t\t\t\t<td width=\"29%\"><font face=\"Times New Roman\" size=\"2\">", "");
+//	      		    str = str.replace("\t\t\t\t<td width=\"29%\" align=\"left\"><font face=\"Times New Roman\" size=\"2\">", "");
+//	      		    str = str.replace("</FONT></td>", "");
+//	      		    str = str.replace("</FONT>", "");
+//	      		    str = str.replace("<br/>", "");
+//	      		    str = str.replace("\t", "");
+//	      		    str = str.replace("\n", ", ");
+//	      		    str = str.replace(", , ", ", ");
+//	      		    str = str.replace("  ", ", ");
+//	      		    str += "\n";
+//	      		    System.out.println(str);
+//	      		    try {
+//	      		    		if (Error == 0) {
+//	      		    			result.write(str);
+//	      		    		} else {
+//	      		    			result.write("Pas de Titulaires !\n");
+//	      		    		}
+//	      		    }
+//	      		    catch (IOException e)
+//	      		    {
+//	      			   e.printStackTrace();
+//	      		    }
 	      		    TimeUnit.SECONDS.sleep(time);
 	        		}
 	        		reader.close();
 	        		result.close();
-	        		JOptionPane finish;
-	        		finish = new JOptionPane();
-	        		finish.showMessageDialog(null, "Un fichier resultats.csv vient d'etre cree sur votre bureau.", "Operation terminee", JOptionPane.INFORMATION_MESSAGE);
+	        		new JOptionPane();
+	        		JOptionPane.showMessageDialog(null, "Un fichier resultats.csv vient d'etre cree sur votre bureau.", "Operation terminee", JOptionPane.INFORMATION_MESSAGE);
 
 
 	         }
